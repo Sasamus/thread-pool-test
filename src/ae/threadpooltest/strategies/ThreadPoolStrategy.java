96bac29fd3ad12b1d58c09e31f2d00d5d8151600
Sasamus <labus93@gmail.com>
2014-11-24 17:09:02 +0100

    Added ThreadPoolStrategy

---
+++ src/ae/threadpooltest/strategies/ThreadPoolStrategy.java
@@ -0,0 +1,22 @@
+/*
+ *  Created: 2014-11-24
+ * Modified: 2014-11-24
+ */
+
+package ae.threadpooltest.strategies;
+
+/**
+ * @author Albin Engström
+ */
+public class ThreadPoolStrategy extends ExecutionStrategy {
+
+	/* (non-Javadoc)
+	 * @see ae.threadpooltest.strategies.ExecutionStrategy#RunIt()
+	 */
+	@Override
+	void RunIt() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
