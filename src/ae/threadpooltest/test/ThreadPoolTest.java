6fae90c06a9776c2309adcd2ed25b98bc504494b
Sasamus <labus93@gmail.com>
2014-11-24 16:23:13 +0100

    Added ThreadPoolTest

---
+++ src/ae/threadpooltest/test/ThreadPoolTest.java
@@ -0,0 +1,17 @@
+package ae.threadpooltest.test;
+
+/**
+ * @author Albin Engström
+ * @since 2014-11-24
+ */
+public class ThreadPoolTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
